38b82d18ce32920c9b0893ac00709bf7e51d85b5
Marcus ParkerGarruba <mparkergarruba@gmail.com>
2022-07-21 13:50:53 -0400

    commit tree 8396d5f3bcd8d3ad41734237da9df3fcdf2d0bb4

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,9 @@
 class Person
-  # your code here
+  attr_accessor 
+  def initialize (att)
+    att.each do |k, v|
+      self.class.attr_accessor(k)
+      self.send("#{k}=", v)
+    end
+  end
 end
